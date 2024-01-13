6a5c5fab319ef79e9ca84a8845f380d182ec4444
Sarp Yavuz <sarpyavuzzz@gmail.com>
2024-01-13 13:42:45 -0800

    Wordle With Color
    Green = Correct letter, correct spot
    Yellow = Correct letter, wrong spot
    Red = Wrong letter, wrong spot

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -1,4 +1,4 @@
-﻿// Wordle Program (No Colours or Includes)
+﻿// Wordle Program (Color Implemented)
 
 int lim = 5;
 
@@ -8,12 +8,13 @@ string[] words = File.ReadAllLines("5_letter_words");
 int index = random.Next(words.Count());
 string word = words[index];
 word = word.ToUpper();
-// Console.WriteLine(word); // correctly takes a word randomly
+//Console.WriteLine(word); // correctly takes a word randomly
 
 char[] wordOrder = word.ToCharArray();
 int wordLength = word.Length;
-Console.WriteLine(wordLength); // test word length gives 5?
+/*
 string currentDisplay;
+
 
 for (int i = 0; i < lim; i++) {                      // Each iteration of the guesses
     currentDisplay = String.Empty;        // Destroy currentDisplay with each iteration and rebuild, display
@@ -37,7 +38,7 @@ for (int i = 0; i < lim; i++) {                      // Each iteration of the gu
 
     for (int j = 1; wordLength >= j; j++){           // Checking each letter and displaying correct and incorrect
         // Console.WriteLine(wordOrder + " " + inputOrder + " ");
-        if (Char.Equals(wordOrder[j-1], inputOrder[j-1])){         // indexOutOfRanges
+        if (Char.Equals(wordOrder[j-1], inputOrder[j-1])){
             currentDisplay = currentDisplay + inputOrder[j-1];  // Add current letter to currentDisplay
         }
         else{
@@ -47,3 +48,50 @@ for (int i = 0; i < lim; i++) {                      // Each iteration of the gu
     
     Console.WriteLine(currentDisplay);
 }
+*/
+
+for (int j = 0; j < lim; j++) {                      // Each guess iteration
+    Console.WriteLine();
+    Console.WriteLine("Guess a 5 letter word");
+    string input = Console.ReadLine();
+    input = input.ToUpper();
+    char[] inputOrder = input.ToCharArray();
+
+    if (input.Length != word.Length){
+        Console.WriteLine("Guess a word with same length next time");
+        break;
+    }
+    if (input == word){
+        Console.WriteLine("Correct! The word was: " + word);
+        break;
+    }
+    else if (j == 4){
+        Console.WriteLine("The correct word was: " + word);
+        break;
+    }
+
+    
+    for (int k = 1; k <= wordLength; k++) {
+        int pos = Array.IndexOf(wordOrder, inputOrder[k-1]);
+        //Console.WriteLine("wordOrder[k-1]: " + wordOrder[k-1] + " " + "inputOrder[k-1]" + inputOrder[k-1] + " pos: " + pos);
+        if (Char.Equals(wordOrder[k-1], inputOrder[k-1])) {  
+            Console.BackgroundColor = ConsoleColor.Black;      
+            Console.ForegroundColor = ConsoleColor.Green; 
+            Console.Write(inputOrder[k-1]);
+        }
+        else if (pos > -1) {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Yellow; 
+            Console.Write(inputOrder[k-1]);
+        }
+        else {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red; 
+            Console.Write("-");
+        }
+        
+    }
+    Console.ResetColor();
+    
+}
+
